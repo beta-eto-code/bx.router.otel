bfc96f2ef4d5311b75e4586630b9d16170996bc9
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-03-26 17:38:22 +0500

    feature: impl handler

--- lib/bxotellisnter.php
+++ lib/bxotellisnter.php
@@ -2,16 +2,28 @@
 
 namespace Bx\Otel\Router;
 
+use Bitrix\Main\Application;
+use Otel\Base\Tests\Stubs\OTelFactoryStub;
+
 class BxOTelListener
 {
+    private static $otelManager;
 
     public static function onStart()
     {
+        $request = Application::getInstance()->getContext()->getRequest();
+        self::$otelManager = (new OTelFactoryStub())->createDefault();
 
+        self::$otelManager->startRootSpan([
+            'http.method' => $request->getRequestMethod(),
+            'http.url' => $request->getRequestUri()
+        ]);
+
+        self::$otelManager->getSpan()->addEvent('onStart', []);
     }
 
     public static function onEnd()
     {
-
+        self::$otelManager->getSpan()->addEvent('onEnd', []);
     }
 }
---
+++ lib/configlist.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bx\Otel\Router;
+
+class ConfigList
+{
+
+    const USE_OTEL = 'USE_OTEL';
+    const OTEL_URLS = 'OTEL_URLS';
+}
