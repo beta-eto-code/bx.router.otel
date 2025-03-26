6dd2f20862202d7b61b03db5529cb788e251dbb3
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-03-26 22:42:36 +0500

    feature: impl options

--- lib/otelmanager.php
+++ lib/otelmanager.php
@@ -7,7 +7,7 @@ use Otel\Base\OTelSpanManager;
 
 class OTelManager
 {
-    private static OTelSpanManager $instance;
+    private static ?OTelSpanManager $instance = null;
 
     private function __construct()
     {
@@ -15,7 +15,7 @@ class OTelManager
 
     public static function getInstance(): OTelSpanManager
     {
-        if (!self::$instance) {
+        if (is_null(self::$instance)) {
             $factory = new OTelFactory();
             $otelSpanManager = $factory->createDefault();
             self::$instance = $otelSpanManager;
