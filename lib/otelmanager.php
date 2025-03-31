26fb12b55751c61e7ef607e4c4d552b540e7824f
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-03-31 10:44:39 +0500

    feature: impol registry

--- lib/otelmanager.php
+++ lib/otelmanager.php
@@ -19,10 +19,15 @@ class OTelManager
 
         if (is_null(self::$instance)) {
             if (!OTelRegistry::has('default')) {
+                self::$instance = OTelRegistry::get('default');
+            } else {
                 $factory = new OTelFactory();
                 $otelSpanManager = $factory->createDefault();
+                $otelSpanManager->startRootSpan();
+
                 self::$instance = $otelSpanManager;
             }
+
         }
 
         return self::$instance;
